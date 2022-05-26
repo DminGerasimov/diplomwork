efe29cbac1a7226618f3a81ac8c9deea5eb7fbae
Dmitry Gerasimov <nsmg@yandex.ru>
2022-05-27 00:00:50 +0300

    Adding filter to VideoClip model

--- videohosting/serializers.py
+++ videohosting/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from models import Video_clip
+from videohosting import models
 
 
 class Video_clip_serializer(serializers.ModelSerializer):
     class Meta:
-        model = Video_clip
+        model = models.Video_clip
         fields = ['title', 'description', 'create_time', 'user']
