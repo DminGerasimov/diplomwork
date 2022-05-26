c1c82117092096831459d36fec470a4f419882f2
Dmitry Gerasimov <nsmg@yandex.ru>
2022-05-26 23:47:10 +0300

    Adding filter to VideoClip model

--- videohosting/views.py
+++ videohosting/views.py
@@ -1,9 +1,8 @@
 from rest_framework import viewsets
-from models import Video_clip
-from videohosting import serializers
+from videohosting import models, serializers
 
 class Video_clip_view_set(viewsets.ModelViewSet):
-    queryset = Video_clip.objects.all()
+    queryset = models.Video_clip.objects.all()
     serializer_class = serializers.Video_clip_serializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['user', 'create_time']
