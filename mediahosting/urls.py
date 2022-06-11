fa2891ed3254ab81ab8335fb4a38db37a37de661
DminGerasimov <nsmg@yandex.ru>
2022-06-11 22:26:30 +0300

    Editing main urls.py (swagger-ui)

--- mediahosting/urls.py
+++ mediahosting/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/', include('videohosting.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
